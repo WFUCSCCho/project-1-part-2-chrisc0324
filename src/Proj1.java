3a57d25b3bf6b4fa43713ab2e2f27ade1ab375be
chrisc0324 <chah22@wfu.edu>
2025-09-25 00:47:29 -0400

    Final submission

--- src/Proj1.java
+++ src/Proj1.java
@@ -1,11 +1,17 @@
+/**
+ * @file: Proj1.java
+ * @description: This class runs the project where it takes the input file and constructs a Parser
+ * @author: Chris Cha {@literal <chah22@wfu.edu>}
+ * @date: September 17, 2025 (Modified September 24, 2025)
+ */
 import java.io.FileNotFoundException;
 
 public class Proj1 {
     public static void main(String[] args) throws FileNotFoundException{
-        if(args.length != 1){
+        if(args.length != 2){
             System.err.println("Argument count is invalid: " + args.length);
             System.exit(0);
         }
-        new Parser(args[0]);
+        new Parser(args[0],args[1]);
     }
 }
